b8d329c5b813535d294e563371468d7d1efcf95c
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-08-28 06:22:49 -0400

    Complete taskList model

---
+++ app/models/TaskListInMemoryModel.scala
@@ -0,0 +1,35 @@
+package models
+
+import scala.collection.mutable
+
+object TaskListInMemoryModel {
+  private val users = mutable.Map[String, String]("User1" -> "Pass1")
+  private val tasks = mutable.Map[String, List[String]]("User1" -> List("Make videos", "eat", "sleep", "code"))
+
+  def validateUser(username: String, password: String): Boolean = {
+    users.get(username).contains(password)
+  }
+
+  def createUser(username: String, password: String): Boolean = {
+    if (users.contains(username)) false else {
+      users(username) = password
+      true
+    }
+  }
+
+  def getTasks(username: String): Seq[String] = {
+    tasks.getOrElse(username, Nil)
+  }
+
+  def addTask(username: String, task: String): Unit = {
+    tasks(username) = task :: tasks.getOrElse(username, Nil)
+  }
+
+  def removeTask(username: String, index: Int): Boolean = {
+    if (index < 0 || !tasks.contains(username) || index >= tasks(username).length) false
+    else {
+      tasks(username) = tasks(username).patch(index, Nil, 1)
+      true
+    }
+  }
+}
